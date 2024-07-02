29428a34285bd4019b002652e3fb1a09096f747e
pvKuznecov <p.v.kuznecov@yandex.ru>
2024-07-02 13:28:01 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-<<<<<<< HEAD
-  publicPath: process.env.NODE_ENV === "production" ? "/Vue-Firebase/" : "/",
-  outputDir: "docs",
-})
-=======
   publicPath: process.env.NODE_ENV === "production" ? "/itdata/" : "/",
   outputDir: "docs",
 })
->>>>>>> a2d51ff1600621f7537b2c9bd4cbb7a54ca64b94
