a3b0596b34961176c58f16e927185b3fee457c34
kup <p.v.kuznecov@yandex.ru>
2024-08-01 14:04:28 +0700

    Template

--- src/views/JsframeworksView/JsframeworksView.vue
+++ src/views/JsframeworksView/JsframeworksView.vue
@@ -3,10 +3,31 @@
 <script>
     export default {
         name: "JsframeworksView",
+        inject: ["GDATA"],
         data() {
             return {
-                areaName: "JS-фреймворки"
+                areaName: "JS-фреймворки",
+                jsfData: {},
             }
         },
+        computed: {
+            AGDATA: function () {
+                return this.GDATA.areaList;
+            },
+            JSFDATA: function () {
+                var res;
+                for (let i=0; i < this.AGDATA.length; i++) {
+                    if (this.AGDATA[i].txtKey == "js-frameworks") {
+                        res = this.AGDATA[i].subarea;
+                    }
+                }
+                return res;
+            }
+        },
+        methods: {
+            getImgByKey(keyVal) {
+                return this.GDATA[keyVal].logo;
+            }
+        }
     }
 </script>
