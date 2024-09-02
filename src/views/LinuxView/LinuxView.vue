93549608d89a1a3d7983343060ed2b15e680ea32
kup <p.v.kuznecov@yandex.ru>
2024-09-02 14:05:04 +0700

    Templ

--- src/views/LinuxView/LinuxView.vue
+++ src/views/LinuxView/LinuxView.vue
@@ -15,6 +15,15 @@
             AGDATA: function () {
                 return this.GDATA.linux;
             },
+            COMMANDSDATA: function () {
+                return this.AGDATA.commands;
+            }
+        },
+        methods: {
+            chgVisiblStatus(elId) {
+                var el = document.getElementById(elId);
+                el.style.display = el.style.display === 'none' ? 'block' : 'none';
+            }
         }
     }
 </script>
